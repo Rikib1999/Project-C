60dcdc837630076763c253bd66f8d8e69b2d5d4f
Rikib1999 <74504682+Rikib1999@users.noreply.github.com>
2023-03-13 23:03:52 +0100

    Initial commit

---
+++ Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmLight : MonoBehaviour
+{
+    public Light lightL;
+
+    void Start()
+    {
+        StartCoroutine(Light());
+    }
+
+    IEnumerator Light()
+    {
+        for (float f = 6f; f >= 1f; f -= 0.2f)
+        {
+            lightL.intensity = f;
+            yield return new WaitForSeconds(0.015f);
+        }
+        for (float f = 1f; f <= 6f; f += 0.2f)
+        {
+            lightL.intensity = f;
+            yield return new WaitForSeconds(0.015f);
+        }
+        StartCoroutine(Light());
+    }
+}
---
+++ Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    public Texture2D cursorTexture1;
+    public Texture2D cursorTexture2;
+    private Vector2 cursorHotspot;
+
+    void Start()
+    {
+        cursorHotspot = new Vector2(cursorTexture1.width / 2, cursorTexture1.height / 2);
+        Cursor.SetCursor(cursorTexture1, cursorHotspot, CursorMode.Auto);
+    }
+
+    void Update()
+    {
+        Vector3 currentMouse = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(currentMouse);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        Debug.DrawLine(ray.origin, hit.point);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.SetCursor(cursorTexture1, cursorHotspot, CursorMode.Auto);
+        }/*
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Cursor.SetCursor(cursorTexture1, cursorHotspot, CursorMode.Auto);
+        }*/
+    }
+
+    public void OverButton()
+    {
+        Cursor.SetCursor(cursorTexture2, cursorHotspot, CursorMode.Auto);
+    }
+
+    public void ExitButton()
+    {
+        Cursor.SetCursor(cursorTexture1, cursorHotspot, CursorMode.Auto);
+    }
+}
